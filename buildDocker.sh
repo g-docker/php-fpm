6cf1af121eb99423c73ce95988cd8fae21481b20
gw123 <963353840@qq.com>
2020-05-02 12:21:48 +0800

    修改打包脚本

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,5 +1,7 @@
-tagName='registry.cn-beijing.aliyuncs.com/w_docker/php7.2-fpm:2.0.2'
-tagName='xytschool/php7.2:2.0.2'
+#tagName='registry.cn-beijing.aliyuncs.com/w_docker/php7.2-fpm:2.0.2'
+tagName='xytschool/php7.2:1.0.2'
+latestTagName='xytschool/php7.2:latest'
 docker build -t $tagName --no-cache .
 docker push  $tagName              
-
+docker tag  $tagName $latestTagName
+docker push $latestTagName
