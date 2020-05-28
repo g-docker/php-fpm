0a8194e4d30b432c198c7094f87973f77296607d
gw123 <963353840@qq.com>
2020-05-28 13:30:53 +0800

    修复其他php-fpm 创建文件没有权限的问题

--- buildDocker7.2-apk.sh
+++ buildDocker7.2-apk.sh
@@ -1,6 +1,6 @@
 set -e
 #tagName='registry.cn-beijing.aliyuncs.com/w_docker/php7.2-fpm:2.0.2'
-tagName='xytschool/php7.2:10.1.1'
+tagName='xytschool/php7.2:10.1.3'
 latestTagName='xytschool/php7.2:latest'
 docker build -t $tagName --no-cache -f Dockerfile7.2-apk .
 docker push  $tagName              
